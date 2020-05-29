4ec6941006b2bca0f901ae9e03cf300f20522b57
Choi Jiwon <winning1993@naver.com>
2020-05-29 16:53:35 +0900

    cd 애니메이션

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const playBtn = document.getElementById("playBtn"),
-status = document.getElementById("status")
+status = document.getElementById("status"),
+cdImage = document.getElementById("cdImage")
 
 function handlePlay(){
     status.style.animation = "showStatus 0.3s forwards";
+    cdImage.style.animation = "rotateCD 5s linear infinite"
     playBtn.removeEventListener("click", handlePlay);
     playBtn.addEventListener("click", handlePause);
 }
 
 function handlePause(){
     status.style.animation = "hideStatus 0.3s forwards";
+    cdImage.style.animationPlayState = "paused";
     playBtn.removeEventListener("click", handlePause);
     playBtn.addEventListener("click", handlePlay);
 }
