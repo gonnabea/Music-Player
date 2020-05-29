fa6cb768e474c37e7dc23dff39f0b9d37bf2f8b6
Choi Jiwon <winning1993@naver.com>
2020-05-29 18:55:49 +0900

    파일 다운로드, 시험재생

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const playBtn = document.getElementById("playBtn"),
 status = document.getElementById("status"),
 cdImage = document.getElementById("cdImage")
 
+let playList = {
+    "Don't Let Me Down (Remix)" : "Musics/Don't Let Me Down (feat. Daya) (Hipst3r Edit).wav",
+    "Work From Home (Remix)" : "Musics/Fifth Harmony  feat. Ty Dolla $ign - Work From Home (Dr Gobbers Edit) FINAL.mp3",
+    "Shape Of You (Remix sample)" : "Musics/Shape Of You (Marimba Remix).mp3"
+},
+nowPlaying = new Audio("Musics/Don't Let Me Down (feat. Daya) (Hipst3r Edit).wav")
+
+console.log(playList)
+
 function handlePlay(){
     status.style.animation = "showStatus 0.3s forwards";
     cdImage.style.animation = "rotateCD 5s linear infinite";
     playBtn.innerHTML = "◼";
+    nowPlaying.play()
     playBtn.removeEventListener("click", handlePlay);
     playBtn.addEventListener("click", handlePause);
 }
@@ -14,6 +24,7 @@ function handlePause(){
     status.style.animation = "hideStatus 0.3s forwards";
     cdImage.style.animationPlayState = "paused";
     playBtn.innerHTML = "▶";
+    nowPlaying.pause()
     playBtn.removeEventListener("click", handlePause);
     playBtn.addEventListener("click", handlePlay);
 }
