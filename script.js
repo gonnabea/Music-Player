03c0768fa1d87d0d586a3d7d52f351cbf1193c1b
Choi Jiwon <winning1993@naver.com>
2020-05-29 20:01:40 +0900

    다음 곡, 이전 곡, CD 이미지 갱신 구현

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 const playBtn = document.getElementById("playBtn"),
 status = document.getElementById("status"),
-cdImage = document.getElementById("cdImage")
+cdImage = document.getElementById("cdImage"),
+nextBtn = document.getElementById("nextBtn"),
+preBtn = document.getElementById("preBtn"),
+musicTitle = document.getElementById("musicTitle")
 
-let playList = {
+/* let playList = {
     "Don't Let Me Down (Remix)" : "Musics/Don't Let Me Down (feat. Daya) (Hipst3r Edit).wav",
     "Work From Home (Remix)" : "Musics/Fifth Harmony  feat. Ty Dolla $ign - Work From Home (Dr Gobbers Edit) FINAL.mp3",
     "Shape Of You (Remix sample)" : "Musics/Shape Of You (Marimba Remix).mp3"
-},
-nowPlaying = new Audio("Musics/Don't Let Me Down (feat. Daya) (Hipst3r Edit).wav")
+}, */
+let playList = ["Musics/Don't Let Me Down (feat. Daya) (Hipst3r Edit).wav",
+"Musics/Fifth Harmony  feat. Ty Dolla $ign - Work From Home (Dr Gobbers Edit) FINAL.mp3",
+"Musics/Ed Sheraan x P.A.F.F. x Salvatore Ganacci - Shape Of You.mp3"],
+imageList = ["Images/Don't Let Me Down.jpg","Images/Work From Home.jpg","Images/Shape Of You.jpg"],
+names = ["Don't Let Me Down (Remix)",
+"Work From Home (Remix)",
+"Shape Of You (Remix)"],
+orderIndex = 0,
+nowPlaying = new Audio(playList[orderIndex]);
+
 
 console.log(playList)
 
 function handlePlay(){
     status.style.animation = "showStatus 0.3s forwards";
     cdImage.style.animation = "rotateCD 5s linear infinite";
+    cdImage.src = imageList[orderIndex];
+    musicTitle.innerHTML = names[orderIndex];
     playBtn.innerHTML = "◼";
-    nowPlaying.play()
+    nowPlaying.play();
+    nowPlaying.addEventListener("ended", handleNext)
     playBtn.removeEventListener("click", handlePlay);
     playBtn.addEventListener("click", handlePause);
 }
@@ -29,8 +44,32 @@ function handlePause(){
     playBtn.addEventListener("click", handlePlay);
 }
 
+function handleNext(){
+    handlePause()
+    orderIndex +=1;
+    if(orderIndex >=3){
+        orderIndex = 0;
+    }
+    console.log(orderIndex)
+    nowPlaying = new Audio(playList[orderIndex])
+    handlePlay()
+}
+
+function handlePre(){
+    handlePause()
+    orderIndex -=1;
+    if(orderIndex < 0){
+        orderIndex = 2;
+    }
+    console.log(orderIndex)
+    nowPlaying = new Audio(playList[orderIndex])
+    handlePlay()
+}
+
 function init(){
     playBtn.addEventListener("click", handlePlay);
+    nextBtn.addEventListener("click", handleNext);
+    preBtn.addEventListener("click", handlePre)
 }
 
 init()
